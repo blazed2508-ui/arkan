7aa70753a538da2a1ce69d68c0753223abec682d
blazed2508-ui <blazed2508@gmail.com>
2026-04-24 17:44:19 -0700

    Begin

---
+++ arkan/config.example.js
@@ -0,0 +1,16 @@
+// NEVER commit config.js to GitHub
+
+window.ARKAN_CONFIG = {
+  firebaseApiKey: "YOUR_API_KEY_HERE",
+  firebaseAuthDomain: "YOUR_AUTH_DOMAIN_HERE",
+  firebaseProjectId: "YOUR_PROJECT_ID_HERE",
+  firebaseStorageBucket: "YOUR_STORAGE_BUCKET_HERE",
+  firebaseMessagingSenderId: "YOUR_SENDER_ID_HERE",
+  firebaseAppId: "YOUR_APP_ID_HERE",
+  ceoEmail: "YOUR_CEO_EMAIL_HERE",
+  adminPasswords: {
+    "HASH_1_HERE": "superadmin",
+    "HASH_2_HERE": "sorter",
+    "HASH_3_HERE": "full",
+  }
+};
